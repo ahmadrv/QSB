643814c7afea3c5faed051d34acb8bf295087f8b
ahmadrv <ahmadmahmoodiandarvishani@gmail.com>
2023-03-03 01:30:08 +0330

    Add a bash script to automate Miniconda installation

---
+++ setup.sh
@@ -0,0 +1,13 @@
+# Install expect to install Miniconda automatically
+#sudo apt install expect
+
+# Download Miniconda file
+# wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+
+
+# TODO: Create a better expect script with "sudo autoexpect
+# -f install_miniconda.exp bash Miniconda*.sh"
+
+
+# Install Miniconda
+expect install_miniconda.exp
