596e84807c35458a200d6cdb49a020751cc8a834
ahmadrv <ahmadmahmoodiandarvishani@gmail.com>
2023-06-17 15:16:49 +0330

    add benchmarks.ipynb for logging benchmarks

--- tools/benchmark.py
+++ tools/benchmark.py
@@ -1,7 +1,9 @@
 import subprocess, time
 
 def runtime(cmd):
-    """Run a command and return its runtime in seconds."""
+    """
+    Run a command and return its runtime in seconds.
+    """
     start = time.time()
     subprocess.check_call(cmd, shell=True)
     return time.time() - start
