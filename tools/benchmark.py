96c2bc01cdd4a22ed224c67d49d2600cfd32d5ae
ahmadrv <ahmadmahmoodiandarvishani@gmail.com>
2023-06-18 15:31:53 +0330

    add memory usage benchmark

--- tools/benchmark.py
+++ tools/benchmark.py
@@ -1,4 +1,4 @@
-import subprocess, time
+import subprocess, time, psutil
 
 def runtime(cmd):
     """
@@ -7,3 +7,28 @@ def runtime(cmd):
     start = time.time()
     subprocess.check_call(cmd, shell=True)
     return time.time() - start
+
+def memory_usage(cmd):
+    """
+    Run a command and return its maximum memory usage in kilobytes.
+    """
+    # Run the subprocess
+    process = subprocess.Popen(cmd)
+
+    # Get the process ID
+    pid = process.pid
+
+   # Initialize a variable to track the maximum memory usage
+    max_memory_usage = 0
+
+    # Loop until the subprocess finishes
+    while process.poll() is None:
+        # Get memory usage
+        memory_info = psutil.Process(pid).memory_info()
+        memory_usage = memory_info.rss
+
+        # Update the maximum memory usage if necessary
+        if memory_usage > max_memory_usage:
+            max_memory_usage = memory_usage
+
+    return max_memory_usage
