05c36bea0ce8cad2b661538883794a6aa67c6e6b
ahmadrv <ahmadmahmoodiandarvishani@gmail.com>
2023-06-24 00:26:42 +0330

    Add more filter on inputs and take a test result

--- tools/shell.py
+++ tools/shell.py
@@ -1,9 +1,13 @@
 import supported
 from command import Command
 
-num_qubits = 5
+num_qubits = 40
 num_shots = 1024
 algorithm = supported.algorithms[0]
+
+if algorithm == "deutsch_jozsa":
+    deutsch_jozsa_case = supported.deutsch_jozsa_cases[0]
+
 platform = supported.platforms[0]
 
 if platform == "Qiskit":
@@ -13,8 +17,6 @@ if platform == "Qiskit":
         backend = supported.aer_backends[0]
 
 benchmark = supported.benchmarks[0]
-
-deutsch_jozsa_case = supported.deutsch_jozsa_cases[0]
 
 command = Command(
     num_qubits=num_qubits,
@@ -26,3 +28,17 @@ command = Command(
     benchmark=benchmark,
     deutsch_jozsa_case=deutsch_jozsa_case,
 )
+
+
+def command_generator():
+    for qnum in range(3, num_qubits + 1):
+        yield Command(
+            num_qubits=qnum,
+            num_shots=num_shots,
+            algorithm=algorithm,
+            platform=platform,
+            provider=provider,
+            backend=backend,
+            benchmark=benchmark,
+            deutsch_jozsa_case=deutsch_jozsa_case,
+        )
