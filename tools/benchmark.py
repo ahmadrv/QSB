c847a5697a9eb7c72e5c1a5e60f8536b7219af07
ahmadrv <ahmadmahmoodiandarvishani@gmail.com>
2023-10-13 17:42:36 +0330

    add documentations to benchmark.py

--- tools/benchmark.py
+++ tools/benchmark.py
@@ -3,7 +3,13 @@ import subprocess, time, psutil, command, result
 
 def runtime(*args) -> float:
     """
-    Run a command and return its runtime in seconds.
+    Measures the runtime of a subprocess.
+
+    Args:
+        *args: The command to be executed by the subprocess.
+
+    Returns:
+        The runtime of the subprocess in seconds.
     """
     start = time.time()
     process = subprocess.Popen(args)
@@ -13,7 +19,13 @@ def runtime(*args) -> float:
 
 def memory_usage(*args) -> float:
     """
-    Run a command and return its maximum memory usage in kilobytes.
+    Returns the maximum memory usage of a process in MB.
+
+    Args:
+        *args: A list of arguments to be passed to the subprocess.
+
+    Returns:
+        The maximum memory usage of the process in MB.
     """
     with subprocess.Popen(list(args)) as process:
         pid = process.pid
@@ -26,18 +38,34 @@ def memory_usage(*args) -> float:
             if memory_usage > max_memory_usage:
                 max_memory_usage = memory_usage
 
-        return max_memory_usage / 1048576  # Convert to megabytes
+        return max_memory_usage / 1048576
 
 
 def run(
-    num_qubits,
-    num_shots,
-    algorithm,
-    platform,
-    provider,
-    backend,
-    benchmarks,
+    num_qubits: list[int],
+    num_shots: list[int],
+    algorithm: list[str],
+    platform: list[str],
+    provider: list[str],
+    backend: list[str],
+    benchmarks: list[str],
 ):
+    """
+    Runs benchmarks for quantum algorithms on different platforms and providers.
+
+    Args:
+        num_qubits (list[int]): List of number of qubits to run the benchmarks on.
+        num_shots (list[int]): List of number of shots to run the benchmarks for.
+        algorithm (list[str]): List of quantum algorithms to run the benchmarks for.
+        platform (list[str]): List of platforms to run the benchmarks on.
+        provider (list[str]): List of providers to run the benchmarks on.
+        backend (list[str]): List of backends to run the benchmarks on.
+        benchmarks (list[str]): List of benchmarks to run.
+
+    Returns:
+        None
+    """
+   
     commands = command.command_generator(
         num_qubits,
         num_shots,
