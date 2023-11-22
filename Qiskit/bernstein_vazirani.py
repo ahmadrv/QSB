57e15e14391bf7d29bce87fac599879787077d6a
ahmadrv <ahmadmahmoodiandarvishani@gmail.com>
2023-11-22 12:33:44 +0330

    first completed version of Qiskit/bernstein_vazirani.py

--- Qiskit/bernstein_vazirani.py
+++ Qiskit/bernstein_vazirani.py
@@ -1,59 +1,69 @@
 from qiskit import QuantumCircuit, transpile
+
 from tools.provider import get_backend
 from tools.interface import args
+
 import random
 
-def bv_oracle(n: int) -> QuantumCircuit:
-    
-    oracle = QuantumCircuit(n + 1)
-    
-    s = f'{random.getrandbits(n):=0{n}b}'
-    
-    s = s[::-1]         # reverse s to fit qiskit's qubit ordering
+def main():
+    """
+    Executes the Bernstein-Vazirani algorithm using the specified arguments.
+    """
+    oracle_gate = bernstein_vazirani_oracle(args.num_qubits)
+    circuit = bernstein_vazirani_algorithm(oracle_gate, args.num_qubits)
+    backend = get_backend(args.provider, args.backend)
+    transpiled_circuit = transpile(circuit, backend)
+    backend.run(
+        transpiled_circuit, shots=args.num_shots
+    ).result()
+
+def bernstein_vazirani_oracle(n: int) -> QuantumCircuit:
+    """
+    Creates a quantum circuit representing the Bernstein-Vazirani oracle.
+
+    Args:
+        n (int): The number of qubits in the circuit.
+
+    Returns:
+        QuantumCircuit: The quantum circuit representing the oracle.
+    """
+    qc = QuantumCircuit(n + 1)
+
+    s = f"{random.getrandbits(n):=0{n}b}"
+
+    s = s[::-1]
     for q in range(n):
-        if s[q] == '0':
-            oracle.i(q)
+        if s[q] == "0":
+            qc.i(q)
         else:
-            oracle.cx(q, n)
-    
-    oracle_gate = oracle.to_gate()
+            qc.cx(q, n)
+
+    oracle_gate = qc.to_gate()
     oracle_gate.name = "Oracle"
     return oracle_gate
 
-def bv_algorithm(oracle: QuantumCircuit, n: int) -> QuantumCircuit:
+def bernstein_vazirani_algorithm(oracle: QuantumCircuit, n: int) -> QuantumCircuit:
     """
-    This function creates a Bernstein-Vazirani algorithm based on the input oracle
-    and input qubit size n.
+    Implements the Bernstein-Vazirani algorithm.
+
+    Args:
+        oracle (QuantumCircuit): The oracle circuit representing the function f(x).
+        n (int): The number of qubits used to represent the input x.
+
+    Returns:
+        QuantumCircuit: The circuit implementing the Bernstein-Vazirani algorithm.
     """
-    bv_circuit = QuantumCircuit(n + 1, n)
-    bv_circuit.h(n)
-    bv_circuit.z(n)
+    qc = QuantumCircuit(n + 1, n)
+    qc.h(n)
+    qc.z(n)
 
-    for qubit in range(n):
-        bv_circuit.h(qubit)
+    qc.h(range(n))
+    qc.append(oracle, range(n + 1))
+    qc.h(range(n))
 
-    bv_circuit.barrier()
-    
-    bv_circuit.append(oracle, range(n + 1))
-    
-    bv_circuit.barrier()
+    qc.measure(range(n), range(n))
 
-    for qubit in range(n):
-        bv_circuit.h(qubit)
-
-    for i in range(n):
-        bv_circuit.measure(i, i)
-
-    return bv_circuit
+    return qc
 
 if __name__ == "__main__":
-    oracle_gate = bv_oracle(args.num_qubits)
-    circuit = bv_algorithm(oracle_gate, args.num_qubits)
-    
-    backend = get_backend(args.provider, args.backend)
-    
-    transpiled_circuit = transpile(circuit, backend)
-    
-    backend.run(transpiled_circuit, shots=args.num_shots).result()      # [x]: add result() based on https://github.com/Qiskit/qiskit-aer/issues/1210
-    
-    # [ ]: Print results to pass outputs to the parent on the subprocess
+    main()
