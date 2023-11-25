2519a7863f4a35885d1e38860003f5283c49ce47
ahmadrv <ahmadmahmoodiandarvishani@gmail.com>
2023-11-25 22:46:20 +0330

    Adding documentation to Qiskit/simon.py

--- Cirq/simon.py
+++ Cirq/simon.py
@@ -8,6 +8,9 @@ import random
 
 
 def main():
+    """
+    Executes the Simon's algorithm.
+    """
     qubits = LineQubit.range(2 * args.num_qubits)
     oracle = simon_oracle(qubits)
     simon_algorithm(qubits, oracle)
@@ -15,28 +18,40 @@ def main():
 
 def simon_oracle(qubits):
     """
-    Creates a Simon oracle circuit.
+    Implements the Simon's algorithm oracle.
 
     Args:
-        n (int): The number of qubits.
+        qubits (List[cirq.Qid]): The qubits to apply the oracle on.
 
     Returns:
-        cirq.Circuit: The Simon oracle circuit.
+        cirq.MatrixGate: The matrix gate representing the oracle operation.
     """
     n = args.num_qubits
     secret_string = f"{random.getrandbits(n):0{n}b}"
     print(secret_string)
-    permuts = np.random.permutation(2 ** n)
-    query_op = np.zeros((4**n, 4 **n))
-    for x in range(2 ** n):
-        for y in range(2 ** n):
+    permuts = np.random.permutation(2**n)
+    query_op = np.zeros((4**n, 4**n))
+    for x in range(2**n):
+        for y in range(2**n):
             z = y ^ permuts[min(x, x ^ int(secret_string, 2))]
-            query_op[x + 2 ** n * z, x + 2 ** n * y] = 1
+            query_op[x + 2**n * z, x + 2**n * y] = 1
 
     return MatrixGate(query_op).on(*qubits[n:], *qubits[:n])
 
 
 def simon_measurements(qubits, oracle):
+    """
+    Perform Simon's algorithm measurements on the given qubits using the
+    specified oracle.
+
+    Args:
+        qubits (List[cirq.Qid]): The qubits to perform measurements on.
+        oracle (cirq.Gate): The oracle gate to apply.
+
+    Returns:
+        List[int]: The measurement results.
+
+    """
     n = args.num_qubits
     qc = Circuit()
     qc.append([H.on_each(*qubits[:n])])
@@ -51,6 +66,16 @@ def simon_measurements(qubits, oracle):
 
 
 def simon_algorithm(qubits, oracle):
+    """
+    Implements the Simon's algorithm.
+
+    Args:
+        qubits (list): List of qubits to be used in the algorithm.
+        oracle (callable): Oracle function that encodes the secret string.
+
+    Returns:
+        str: The secret string obtained from the algorithm.
+    """
     measurements = simon_measurements(qubits, oracle)
     matrix = np.array(measurements).astype(int)
     null_space = galois.GF(2)(matrix).null_space()
