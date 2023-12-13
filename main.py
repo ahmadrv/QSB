554dc4d6ccdefc1dd845573d0434b264cc303928
ahmadrv <ahmadmahmoodiandarvishani@gmail.com>
2023-12-13 11:09:58 +0330

    postpone getting input from user

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-from tools import interface
+from tools import interface, benchmark
+
 
 def main():
     QSB = interface.Interface()
-    print(QSB.get_user_inputs())
-    # platform, provider, backend, algorithm, benchmark, num_qubits, num_shots = QSB.get_user_inputs()
+    (
+        platform,
+        provider,
+        backend,
+        algorithm,
+        benchmark,
+        num_qubits,
+        num_shots,
+    ) = QSB.get_user_inputs()
 
+    # [ ]: It will be completed.
+    
 if __name__ == "__main__":
     main()
