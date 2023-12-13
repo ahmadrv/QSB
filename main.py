8749d1db11eddfd2570aa3e91a2c79eccb5ae89e
ahmadrv <ahmadmahmoodiandarvishani@gmail.com>
2023-12-13 13:48:18 +0330

    prepare for new approach

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
-from tools import interface, benchmark
+from tools import benchmark
 
 
 def main():
-    QSB = interface.Interface()
-    (
-        platform,
-        provider,
-        backend,
-        algorithm,
-        benchmark,
-        num_qubits,
-        num_shots,
-    ) = QSB.get_user_inputs()
+    expr_rept = 1
+    num_qubits = list(range(2, 5))
+    num_shots = [1]
+    algorithms = [
+                "deutsch_jozsa",
+                "bernstein_vazirani",
+                "quantum_fourier_transform",
+                "simon"
+    ]
+    platforms = ["Qiskit"]
+    providers = ["aer"]
+    backends = ["aer_simulator"]
+    
+    benchmarks = ["runtime", "memory_usage"]
 
-    # [ ]: It will be completed.
+    for _ in range(expr_rept):
+        file_name = benchmark.run(
+            num_qubits,
+            num_shots,
+            algorithms,
+            platforms,
+            providers,
+            backends,
+            benchmarks
+        )
     
 if __name__ == "__main__":
     main()
