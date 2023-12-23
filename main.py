6cbfc0b3e415d5ed42e464d04e9ae419212e4ef6
ahmadrv <ahmadrv@protonmail.com>
2023-12-23 16:09:27 +0330

    temperory commit

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ from tools import benchmark
 
 def main():
     expr_rept = 1
-    num_qubits = list(range(29, 30))
+    num_qubits = list(range(2, 50))
     num_shots = [1]
     algorithms = [
                 "deutsch_jozsa",
-                # "bernstein_vazirani",
-                # "quantum_fourier_transform",
-                # "simon"
+                "bernstein_vazirani",
+                "quantum_fourier_transform",
+                "simon"
     ]
     platforms = ["Qiskit"]
     providers = ["aer"]
@@ -16,20 +16,15 @@ def main():
     
     benchmarks = ["runtime", "memory_usage"]
 
-    for _ in range(expr_rept):
-        
-        try:
-        
-            benchmark.run(
-                num_qubits,
-                num_shots,
-                algorithms,
-                platforms,
-                providers,
-                backends,
-                benchmarks
-            )
-        except Exception as e:
-            raise e    
+    for _ in range(expr_rept):        
+        benchmark.run(
+            num_qubits,
+            num_shots,
+            algorithms,
+            platforms,
+            providers,
+            backends,
+            benchmarks
+        ) 
 if __name__ == "__main__":
     main()
